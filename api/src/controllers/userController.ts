cf4316fdfff7e98d782118d75c26360337b2292c
Yash Raj <kyashraj991@gmail.com>
2025-03-27 13:17:17 +0530

    create register/signup function

--- api/src/controllers/userController.ts
+++ api/src/controllers/userController.ts
@@ -1,2 +1,42 @@
-import User from "../model/user.model";
+import User from '../model/user.model';
+import { Request, Response } from 'express';
 
+export const register = async (req: Request, res: Response): Promise<void> => {
+  try {
+
+    const { name, username, password } = req.body;
+
+    if (!name || !username || !password) {
+      res.status(400).json({
+        success: false,
+        message: 'All the fields are required',
+      });
+      return;
+    }
+
+    const existingUser = await User.findOne({ username });
+
+    if (existingUser) {
+      res.status(400).json({
+        success: false,
+        message: 'User already exists',
+      });
+      return;
+    }
+
+    const user = await User.create({ name, username, password });
+
+    res.status(200).json({
+      success: true,
+      user,
+      message: 'New user created',
+    });
+
+  } catch (error) {
+    console.error('Error in SIGNUP ', error);
+    res.status(500).json({
+      success: false,
+      message: 'Server Error',
+    });
+  }
+};
