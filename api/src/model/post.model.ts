5b9bea12a9f43a3da57bef283a9b78f9634e6564
Yash Raj <kyashraj991@gmail.com>
2025-03-27 11:30:19 +0530

    create post blog model

---
+++ api/src/model/post.model.ts
@@ -0,0 +1,35 @@
+import mongoose, { Schema, model } from 'mongoose';
+
+const postSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    tags: {
+      type: [String],
+      default: [],
+    },
+    likes: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = model('Post', postSchema);
+
+export default Post;
--- api/src/model/user.model.ts
+++ api/src/model/user.model.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcrypt';
 import { Schema, model } from 'mongoose';
 
 const userSchema = new Schema(
@@ -16,24 +16,22 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
-    isAdmin: {
-      type: Boolean,
-      default: false,
-    },
+
     profilePicture: {
       type: String,
-      default: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+      default:
+        'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
     },
   },
   { timestamps: true }
 );
 
-userSchema.pre("save", async function(next) {
-  if(!this.isModified("password")) return next;
-  const salt = await bcrypt.genSalt(10)
-  this.password = await bcrypt.hash(this.password, salt)
-  next()
-})
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
 const User = model('User', userSchema);
 
