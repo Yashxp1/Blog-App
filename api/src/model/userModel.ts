4e559ff9e838ea68809b45256015007bc15373fa
Yash Raj <kyashraj991@gmail.com>
2025-03-27 10:54:05 +0530

    user model setup

---
+++ api/src/model/userModel.ts
@@ -0,0 +1,40 @@
+import bcrypt from 'bcrypt'
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    profilePicture: {
+      type: String,
+      default: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function(next) {
+  if(!this.isModified("password")) return next;
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+  next()
+})
+
+const User = model('User', userSchema);
+
+export default User;
