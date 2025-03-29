cb09163e4584aff52157f2bfc9dadeb378498338
Yash Raj <kyashraj991@gmail.com>
2025-03-29 17:42:00 +0530

    create blogs state setup

--- client/src/store/fetchBlogs.ts
+++ client/src/store/fetchBlogs.ts
@@ -5,12 +5,18 @@ import { BaseURL } from './BaseURL';
 
 type BlogStore = {
   blogs: BlogPost[];
-
   getBlogs: () => Promise<void>;
+  createBlog: (
+    title: string,
+    content: string,
+    image?: string,
+    tags?: string[]
+  ) => Promise<boolean>;
 };
 
 export const useBlogStore = create<BlogStore>((set) => ({
   blogs: [],
+
   getBlogs: async () => {
     try {
       const token = localStorage.getItem('token');
@@ -29,6 +35,38 @@ export const useBlogStore = create<BlogStore>((set) => ({
       set({ blogs: response.data.getPosts });
     } catch (error) {
       console.error('Error fetching blogs:', error);
+    }
+  },
+
+  createBlog: async (title, content, image = '', tags = []) => {
+    try {
+      const token = localStorage.getItem('token');
+
+      if (!token) {
+        console.error('No authentication token found');
+        return false;
+      }
+      const res = await axios.post(
+        `${BaseURL}/blogs/create`,
+        { title, content, image, tags },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      if (res.status === 201) {
+        set((state) => ({
+          blogs: [...state.blogs, res.data.newPost],
+        }));
+        return true;
+      }
+      return false;
+    } catch (error) {
+      console.error('No authentication token found');
+      return false;
     }
   },
 }));
